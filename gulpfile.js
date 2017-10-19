c53b5aada931aa24434dcb502fc4aa756af6d0c0
michaelujkim <michaelujkim@gmail.com>
2017-10-19 14:58:19 -0700

    oct19

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp'),
 uglify = require('gulp-uglify'),
 rename = require('gulp-rename'),
-browserSync = require('browser-sync');
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+browserSync = require('browser-sync'),
+eslint = require('gulp-eslint'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+cssnano = require('gulp-cssnano'),
+prettyError = require('gulp-prettyerror');
 
-
+gulp.task('sass', function() {
+  gulp.src('./sass/style.scss')
+    .pipe(prettyError()) 
+     .pipe(sass())
+     .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+     }))
+     .pipe(gulp.dest('./build/css'))
+     .pipe(cssnano())
+     .pipe(rename('style.min.css'))
+     .pipe(gulp.dest('./build/css'));
+});
 
 gulp.task('scripts',['lint'], function(){
 gulp.src('./js/*.js')
@@ -29,6 +43,7 @@ gulp.task('lint', function(){
 
 
 gulp.task('watch', function() {
+  gulp.watch('sass/*.scss',['sass']);
   gulp.watch('js/*.js', ['scripts']);
 });
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-var hello = console.log('hi');
+// Built by LucyBot. www.lucybot.com
 
-hello();
-this
+$("#sections").on('change',function(){
+  
+  
+  var value = $("#sections").val();
+  // console.log(value);
+
+var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
+url += '?' + $.param({
+  'api-key': "5bce6c6ed27244808f1ac64af9f33203"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(data) {
+  $.each(data.results, function(index,value){
+$('.story-grid').append(value.multimedia[0].url);
+
+  });
+  console.log(data.results[0].title);
+
+}).fail(function(err) {
+  throw err;
+});
+
+});
+
+
+
