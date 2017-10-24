4c389e37276aa26b9034625c16431e16c58bc99b
michaelujkim <michaelujkim@gmail.com>
2017-10-24 06:03:49 -0700

    typed up readme

---
+++ README.md
@@ -0,0 +1,17 @@
+Instanews Website
+
+this is our second project for Red academy. It is a reader site that features the NYT api.
+
+
+Project Features.
+-html
+-css
+-flexbox
+-jquery
+-javascript
+-ajax
+-git
+-gulp
+-sass
+-json
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 // Built by LucyBot. www.lucybot.com
 
 $("#sections").on('change',function(){
+
     var value = $("#sections").val();
       // console.log(value);
     var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
         url += '?' + $.param({
         'api-key': "5bce6c6ed27244808f1ac64af9f33203"
 });
+
+// $.ajaxStart(function(){
+// $('.loader').addClass('loading');
+// });
 
 $.ajax({
   url: url,
@@ -18,29 +23,24 @@ $.ajax({
   $.each(data.results, function(index,value){
     var photos = value.multimedia[4].url;
     console.log(photos);
-    $('.story-grid').append(
+    $('.story-grid').slice(0,11).append(
   // '<ul><img class="stories" src="'+value.multimedia[4].url+'"></li>',
-  '<ul style="background-image: url('+photos+');"  class="stories"><span class="caption">"'+value.title+'"<span></ul>' );
+  '<li style="background-image: url('+photos+');"  class="stories"><span class="caption">"'+value.title+'"<span></li>' );
 
-
-
-  
-
-  console.log(data.results[0].title);
-
-})
+  })
+ 
 
 .fail(function(err) {
   throw err;
 })
 
 .always(function(){
-  
+ 
   
 });
-
+$('ul').slice(0,11);
 
 })
-$('.story-grid').slice(0,12);
+
 
 });
