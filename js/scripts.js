d8778bbbfaf71ecc1ee814a74f47369ab73297d1
Amelia <ameliagarvin1@gmail.com>
2017-10-26 19:00:21 -0700

    Loading gif and cleaned up Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,37 @@
 // Built by LucyBot. www.lucybot.com
 
 $("#sections").on('change',function(){
-
+    $('.loader').show();// When user clicks on menu loading gif appears
+    var loadingGif = $('.loader');
     var value = $("#sections").val();
-      // console.log(value);
-    var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
-        url += '?' + $.param({
-        'api-key': "5bce6c6ed27244808f1ac64af9f33203"
-});
+   
+    var url = "https://api.nytimes.com/svc/topstories/v2/";
+    url += value;
+    url += ".json?";
+    url += $.param({
+      'api-key': "5bce6c6ed27244808f1ac64af9f33203"
+    });
 
-// $.ajaxStart(function(){
-// $('.loader').addClass('loading');
-// });
-
-$.ajax({
-  url: url,
-  method: 'GET',
-})
-.done(function(data){
-  $('.story-grid').empty();
-  // $.slice(1,12);
-  $.each(data.results, function(index,value){
-    var photos = value.multimedia[4].url;
-    console.log(photos);
-    $('.story-grid').slice(0,11).append(
-  // '<ul><img class="stories" src="'+value.multimedia[4].url+'"></li>',
-  '<li style="background-image: url('+photos+');"  class="stories"><span class="caption">"'+value.title+'"<span></li>' );
-
-  })
- 
-
-.fail(function(err) {
-  throw err;
-})
-
-.always(function(){
- 
+    $.ajax({
+      url: url,
+      method: 'GET',
+    })
+    .done(function(data){
+      
+      $('.story-grid').empty();
+        
+      $.each(data.results, function(index,value){
+        var photos = value.multimedia[4].url;
   
-});
-$('ul').slice(0,11);
-
+      $('.story-grid').slice(0,11).append(
+      
+      '<li style="background-image: url('+photos+');"  class="stories"><span class="caption">"'+value.title+'"<span></li>' );
+    
+      })
+    .fail(function(err) {
+      throw err;
+    })
+    .always(function(){
+    });
+    })
 })
-
-
-});
